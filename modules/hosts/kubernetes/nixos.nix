066712045042f494c494ba533e63cfb376ec09b7
Thomas Nicollet <thomas.nicollet@nebulis.io>
2025-12-23 10:55:13 +0100

    :sparkles: Added certs and manifest for etcd

--- modules/hosts/kubernetes/nixos.nix
+++ modules/hosts/kubernetes/nixos.nix
@@ -100,6 +100,8 @@ in
         };
       };
 
+      # ip addr show tailscale0
+
       systemd.services = {
         create-apiserver-kubelet-config = {
           path = [
@@ -143,6 +145,197 @@ in
           '';
         };
 
+        create-etcd-manifest = {
+          path = [
+            tailscaleCfg.package
+          ];
+
+          enableStrictShellChecks = true;
+          description = "Create Etcd Manifest";
+          documentation = [ "https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet/" ];
+          before = [ "kubelet.service" ];
+          wantedBy = [ "multi-user.target" ];
+
+          serviceConfig = {
+            Type = "oneshot";
+          };
+
+          script = ''
+            if [ ! -f /etc/kubernetes/manifest/etcd.yaml ]; then
+            cat > /etc/kubernetes/manifest/etcd.yaml <<-EOF
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              annotations:
+                kubeadm.kubernetes.io/etcd.advertise-client-urls: https://$(tailscale ip -4):2379
+              labels:
+                component: etcd
+                tier: control-plane
+              name: etcd
+              namespace: kube-system
+            spec:
+              containers:
+              - command:
+                - etcd
+                - --name=${config.networking.hostName}
+                - --data-dir=/var/lib/etcd
+                - --advertise-client-urls=https://$(tailscale ip -4):2379
+                - --listen-client-urls=https://127.0.0.1:2379,https://$(tailscale ip -4):2379
+                - --initial-advertise-peer-urls=https://$(tailscale ip -4):2380
+                - --initial-cluster=${config.networking.hostName}=https://$(tailscale ip -4):2380
+                - --listen-metrics-urls=http://127.0.0.1:2381
+                - --listen-peer-urls=https://$(tailscale ip -4):2380
+                - --client-cert-auth=true
+                - --peer-client-cert-auth=true
+                - --feature-gates=InitialCorruptCheck=true
+                - --snapshot-count=10000
+                - --trusted-ca-file=/etc/kubernetes/pki/etcd/ca.crt
+                - --peer-trusted-ca-file=/etc/kubernetes/pki/etcd/ca.crt
+                - --cert-file=/etc/kubernetes/pki/etcd/server.crt
+                - --key-file=/etc/kubernetes/pki/etcd/server.key
+                - --peer-key-file=/etc/kubernetes/pki/etcd/peer.key
+                - --peer-cert-file=/etc/kubernetes/pki/etcd/peer.crt
+                - --watch-progress-notify-interval=5s
+                image: registry.k8s.io/etcd:3.6.5-0
+                imagePullPolicy: IfNotPresent
+                livenessProbe:
+                  failureThreshold: 8
+                  httpGet:
+                    host: 127.0.0.1
+                    path: /livez
+                    port: probe-port
+                    scheme: HTTP
+                  initialDelaySeconds: 10
+                  periodSeconds: 10
+                  timeoutSeconds: 15
+                name: etcd
+                ports:
+                - containerPort: 2381
+                  name: probe-port
+                  protocol: TCP
+                readinessProbe:
+                  failureThreshold: 3
+                  httpGet:
+                    host: 127.0.0.1
+                    path: /readyz
+                    port: probe-port
+                    scheme: HTTP
+                  periodSeconds: 1
+                  timeoutSeconds: 15
+                resources:
+                  requests:
+                    cpu: 100m
+                    memory: 100Mi
+                startupProbe:
+                  failureThreshold: 24
+                  httpGet:
+                    host: 127.0.0.1
+                    path: /readyz
+                    port: probe-port
+                    scheme: HTTP
+                  initialDelaySeconds: 10
+                  periodSeconds: 10
+                  timeoutSeconds: 15
+                volumeMounts:
+                - mountPath: /var/lib/etcd
+                  name: etcd-data
+                - mountPath: /etc/kubernetes/pki/etcd
+                  name: etcd-certs
+              hostNetwork: true
+              priority: 2000001000
+              priorityClassName: system-node-critical
+              securityContext:
+                seccompProfile:
+                  type: RuntimeDefault
+              volumes:
+              - hostPath:
+                  path: /etc/kubernetes/pki/etcd
+                  type: DirectoryOrCreate
+                name: etcd-certs
+              - hostPath:
+                  path: /var/lib/etcd
+                  type: DirectoryOrCreate
+                name: etcd-data
+            status: {}
+            EOF
+
+              chmod 644 /etc/kubernetes/manifest/etcd.yaml
+            fi
+          '';
+        };
+
+        create-etcd-certs = {
+          path = [
+            tailscaleCfg.package
+            pkgs.openssl
+          ];
+
+          enableStrictShellChecks = true;
+          description = "Create Etcd Certificates";
+          documentation = [ "https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet/" ];
+          before = [ "kubelet.service" ];
+          wantedBy = [ "multi-user.target" ];
+
+          serviceConfig = {
+            Type = "oneshot";
+          };
+
+          script = ''
+            if [ ! -f /etc/kubernetes/pki/etcd/ca.crt ] || [ ! -f /etc/kubernetes/pki/etcd/ca.key ]; then
+              echo "Required Etcd CA is missing, cannot create Etcd certs."
+              exit 1
+            fi
+
+            if [ ! -f /etc/kubernetes/pki/etcd/server.key ]; then
+              openssl genpkey -algorithm ED25519 -out "/etc/kubernetes/pki/etcd/server.key"
+              chmod 600 "/etc/kubernetes/pki/etcd/server.key"
+            fi
+
+            if [ ! -f /etc/kubernetes/pki/etcd/server.crt ] || ! openssl x509 -checkend 86400 -noout -in /etc/kubernetes/pki/etcd/server.crt; then
+              openssl req -new \
+                -key "/etc/kubernetes/pki/etcd/server.key" \
+                -subj "/CN=kube-etcd/O=etcd" \
+                -out "/tmp/etcd-server.csr" \
+                -addext "subjectAltName = DNS:${config.networking.hostName}, IP:$(tailscale ip -4), DNS:localhost, IP:127.0.0.1"
+
+              openssl x509 -req \
+                -in "/tmp/etcd-server.csr" \
+                -CA "/etc/kubernetes/pki/etcd/ca.crt" \
+                -CAkey "/etc/kubernetes/pki/etcd/ca.key" \
+                -out "/etc/kubernetes/pki/etcd/server.crt" \
+                -days 365 \
+                -sha512 \
+                -extfile <(printf "subjectAltName=DNS:${config.networking.hostName}, IP:$(tailscale ip -4), DNS:localhost, IP:127.0.0.1")
+              chmod 644 "/etc/kubernetes/pki/etcd/server.crt"
+              rm -f "/tmp/etcd-server.csr"
+            fi
+
+            if [ ! -f /etc/kubernetes/pki/etcd/peer.key ]; then
+              openssl genpkey -algorithm ED25519 -out "/etc/kubernetes/pki/etcd/peer.key"
+              chmod 600 "/etc/kubernetes/pki/etcd/peer.key"
+            fi
+
+            if [ ! -f /etc/kubernetes/pki/etcd/peer.crt ] || ! openssl x509 -checkend 86400 -noout -in /etc/kubernetes/pki/etcd/peer.crt; then
+              openssl req -new \
+                -key "/etc/kubernetes/pki/etcd/peer.key" \
+                -subj "/CN=kube-etcd-peer/O=etcd" \
+                -out "/tmp/etcd-peer.csr" \
+                -addext "subjectAltName = DNS:${config.networking.hostName}, IP:$(tailscale ip -4), DNS:localhost, IP:127.0.0.1"
+
+              openssl x509 -req \
+                -in "/tmp/etcd-peer.csr" \
+                -CA "/etc/kubernetes/pki/etcd/ca.crt" \
+                -CAkey "/etc/kubernetes/pki/etcd/ca.key" \
+                -out "/etc/kubernetes/pki/etcd/peer.crt" \
+                -days 365 \
+                -sha512 \
+                -extfile <(printf "subjectAltName=DNS:${config.networking.hostName}, IP:$(tailscale ip -4), DNS:localhost, IP:127.0.0.1")
+              chmod 644 "/etc/kubernetes/pki/etcd/peer.crt"
+              rm -f "/tmp/etcd-peer.csr"
+            fi
+          '';
+        };
+
         create-kubelet-kubeconfig = {
           path = [
             pkgs.openssl
@@ -153,6 +346,10 @@ in
           documentation = [ "https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet/" ];
           before = [ "kubelet.service" ];
           wantedBy = [ "multi-user.target" ];
+
+          serviceConfig = {
+            Type = "oneshot";
+          };
 
           script = ''
             if [ ! -f /etc/kubernetes/pki/ca.crt ] || [ ! -f /etc/kubernetes/pki/ca.key ]; then
@@ -183,7 +380,7 @@ in
             - name: kubernetes
               cluster:
                 certificate-authority-data: $(base64 -w0 /etc/kubernetes/pki/ca.crt)
-                server: https://127.0.0.1:6443
+                server: https://$(tailscale ip -4):6443
             contexts:
             - name: system:node:${config.networking.hostName}@kubernetes
               context:

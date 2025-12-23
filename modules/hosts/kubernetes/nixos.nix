6056b8fe6d97538c79b80abb2c6bbc231bb3ed93
Thomas Nicollet <thomas.nicollet@nebulis.io>
2025-12-23 10:08:48 +0100

    :sparkles: Added kubeconfig

--- modules/hosts/kubernetes/nixos.nix
+++ modules/hosts/kubernetes/nixos.nix
@@ -45,7 +45,7 @@ in
           text = builtins.readFile "${inputs.self}/certs/ca-etcd.crt";
           mode = "0644";
         };
-        "kubernetes/kubelet-config.yaml" = {
+        "kubernetes/kubelet/config.yaml" = {
           text = ''
             apiVersion: kubelet.config.k8s.io/v1beta1
             authentication:
@@ -122,6 +122,85 @@ in
               openssl genpkey -algorithm ED25519 -out "/etc/kubernetes/pki/apiserver-kubelet-client.key"
               chmod 600 "/etc/kubernetes/pki/apiserver-kubelet-client.key"
             fi
+
+            if [ ! -f /etc/kubernetes/pki/apiserver-kubelet-client.crt ] || ! openssl x509 -checkend 86400 -noout -in /etc/kubernetes/pki/apiserver-kubelet-client.crt; then
+              openssl req -new \
+                -key "/etc/kubernetes/pki/apiserver-kubelet-client.key" \
+                -subj "/CN=kube-apiserver-kubelet-client/O=kubeadm:cluster-admins" \
+                -out "/tmp/apiserver-kubelet-client.csr"
+
+              openssl x509 -req \
+                -in "/tmp/apiserver-kubelet-client.csr" \
+                -CA "/etc/kubernetes/pki/ca.crt" \
+                -CAkey "/etc/kubernetes/pki/ca.key" \
+                -out "/etc/kubernetes/pki/apiserver-kubelet-client.crt" \
+                -days 365 \
+                -sha512
+
+              chmod 644 "/etc/kubernetes/pki/apiserver-kubelet-client.crt"
+              rm -f "/tmp/apiserver-kubelet-client.csr"
+            fi
+          '';
+        };
+
+        create-kubelet-kubeconfig = {
+          path = [
+            pkgs.openssl
+          ];
+
+          enableStrictShellChecks = true;
+          description = "Create Kubelet kubeconfig";
+          documentation = [ "https://kubernetes.io/docs/reference/command-line-tools-reference/kubelet/" ];
+          before = [ "kubelet.service" ];
+          wantedBy = [ "multi-user.target" ];
+
+          script = ''
+            if [ ! -f /etc/kubernetes/pki/ca.crt ] || [ ! -f /etc/kubernetes/pki/ca.key ]; then
+              echo "Required certs are missing, cannot create kubelet kubeconfig."
+              exit 1
+            fi
+
+            if [ ! -f /etc/kubernetes/kubelet/kubeconfig ]; then
+              openssl genpkey -algorithm ED25519 -out "/tmp/kubelet.key"
+
+              openssl req -new \
+                -key "/tmp/kubelet.key" \
+                -subj "/CN=system:node:${config.networking.hostName}/O=system:nodes" \
+                -out "/tmp/kubelet.csr"
+
+              openssl x509 -req \
+                -in "/tmp/kubelet.csr" \
+                -CA "/etc/kubernetes/pki/ca.crt" \
+                -CAkey "/etc/kubernetes/pki/ca.key" \
+                -out "/tmp/kubelet.crt" \
+                -days 365 \
+                -sha512
+
+              cat > /etc/kubernetes/kubelet/kubeconfig <<-EOF
+                apiVersion: v1
+                kind: Config
+                clusters:
+                - name: kubernetes
+                  cluster:
+                    certificate-authority-data: $(base64 -w0 /etc/kubernetes/pki/ca.crt)
+                    server: https://127.0.0.1:6443
+                contexts:
+                - name: system:node:${config.networking.hostName}@kubernetes
+                  context:
+                    cluster: kubernetes
+                    user: system:node:${config.networking.hostName}
+                current-context: system:node:${config.networking.hostName}@kubernetes
+                users:
+                - name: system:node:${config.networking.hostName}
+                  user:
+                    client-certificate-data: $(base64 -w0 /tmp/kubelet.crt)
+                    client-key-data: $(base64 -w0 /tmp/kubelet.key)
+              EOF
+
+              rm -f "/tmp/kubelet.key" "/tmp/kubelet.csr" "/tmp/kubelet.crt"
+              
+              chmod 600 "/etc/kubernetes/kubelet/kubeconfig"
+            fi
           '';
         };
 
@@ -135,7 +214,8 @@ in
           serviceConfig = {
             ExecStart = ''
               ${pkgs.kubernetes}/bin/kubelet \
-                --config=/etc/kubernetes/kubelet-config.yaml \
+                --config=/etc/kubernetes/kubelet/config.yaml \
+                --kubeconfig=/etc/kubernetes/kubelet/kubeconfig \
                 --v=2
             '';
             Restart = "on-failure";

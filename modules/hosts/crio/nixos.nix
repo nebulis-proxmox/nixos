575a2523caee5a02abe6cd9c24c8755d20dae474
Thomas Nicollet <thomas.nicollet@nebulis.io>
2026-01-31 13:02:38 +0100

    :sparkles: Added calico-ipam to the list of cni plugins

--- modules/hosts/crio/nixos.nix
+++ modules/hosts/crio/nixos.nix
@@ -14,9 +14,7 @@ in
         cri-o
         cri-tools
         cni
-        cni-plugins
         calicoctl # Enable only with K8S ?
-        calico-cni-plugin # Enable only with K8S ?
       ];
 
       environment.etc = {
@@ -49,47 +47,64 @@ in
         };
       };
 
-      systemd.services.crio = {
-        path = [
-          pkgs.cri-o
-          pkgs.mount
-          pkgs.coreutils
-        ];
+      systemd.services.crio =
+        let
+          calico-cni-plugin-ipam = pkgs.calico-cni-plugin.overrideAttrs (
+            final: prev: {
+              pname = prev.pname + "-ipam";
+              installPhase = ''
+                ${prev.installPhase}
+                mv $out/bin/calico $out/bin/calico-ipam
+              '';
+            }
+          );
+        in
+        {
+          path = [
+            pkgs.cri-o
+            pkgs.mount
+            pkgs.coreutils
+            pkgs.cni-plugins
+            pkgs.calico-cni-plugin
+            calico-cni-plugin-ipam
+          ];
 
-        wantedBy = [ "multi-user.target" ];
-        aliases = [ "cri-o.service" ];
+          wantedBy = [ "multi-user.target" ];
+          aliases = [ "cri-o.service" ];
 
-        description = "Container Runtime Interface for OCI (CRI-O)";
-        documentation = [ "https://github.com/cri-o/cri-o" ];
-        wants = [ "network-online.target" ];
-        before = [ "kubelet.service" ];
-        after = [ "network-online.target" ];
+          description = "Container Runtime Interface for OCI (CRI-O)";
+          documentation = [ "https://github.com/cri-o/cri-o" ];
+          wants = [ "network-online.target" ];
+          before = [ "kubelet.service" ];
+          after = [ "network-online.target" ];
 
-        serviceConfig = {
-          Type = "notify";
-          EnvironmentFile = "-/etc/sysconfig/crio";
-          Environment = "GOTRACEBACK=crash";
-          TasksMax = "infinity";
-          LimitNPROC = 1048576;
-          OOMScoreAdjust = -999;
-          TimeoutStartSec = 0;
-          Restart = "on-failure";
-          RestartSec = 10;
-          WatchdogSec = "60s";
-          ExecStart = ''
-            ${pkgs.cri-o}/bin/crio \
-              --cni-plugin-dir=${lib.getBin pkgs.cni-plugins}/bin \
-              $CRIO_CONFIG_OPTIONS \
-              $CRIO_RUNTIME_OPTIONS \
-              $CRIO_STORAGE_OPTIONS \
-              $CRIO_NETWORK_OPTIONS \
-              $CRIO_METRICS_OPTIONS
-          '';
-          ExecReload = ''
-            kill -HUP $MAINPID
-          '';
+          serviceConfig = {
+            Type = "notify";
+            EnvironmentFile = "-/etc/sysconfig/crio";
+            Environment = "GOTRACEBACK=crash";
+            TasksMax = "infinity";
+            LimitNPROC = 1048576;
+            OOMScoreAdjust = -999;
+            TimeoutStartSec = 0;
+            Restart = "on-failure";
+            RestartSec = 10;
+            WatchdogSec = "60s";
+            ExecStart = ''
+              ${pkgs.cri-o}/bin/crio \
+                --cni-plugin-dir=${lib.getBin pkgs.cni-plugins}/bin \
+                --cni-plugin-dir=${lib.getBin pkgs.custom.calico-cni-plugin}/bin \
+                --cni-plugin-dir=${lib.getBin calico-cni-plugin-ipam}/bin \
+                $CRIO_CONFIG_OPTIONS \
+                $CRIO_RUNTIME_OPTIONS \
+                $CRIO_STORAGE_OPTIONS \
+                $CRIO_NETWORK_OPTIONS \
+                $CRIO_METRICS_OPTIONS
+            '';
+            ExecReload = ''
+              kill -HUP $MAINPID
+            '';
+          };
         };
-      };
     })
   ];
 }

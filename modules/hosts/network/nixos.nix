8e1b1004ea69a36f761005318bd4beea1913aded
Thomas Nicollet <thomas.nicollet@nebulis.io>
2026-09-08 22:16:49 +0200

    Remove mkForce on firewall

--- modules/hosts/network/nixos.nix
+++ modules/hosts/network/nixos.nix
@@ -13,7 +13,6 @@ in
 
       networking.firewall = {
         enable = true;
-        allowedTCPPorts = lib.mkForce [ 6443 ];
         allowedUDPPorts = lib.mkForce [ (lib.mkIf config.nebulis.tailscale.enable config.nebulis.tailscale.udpListenPort) ];
         allowPing = true;
         checkReversePath = lib.mkForce false;
